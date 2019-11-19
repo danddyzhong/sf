0dbe0ccda48be39d45ee452018b7b23c671b1881
danddyzhong <1437347093@qq.com>
2019-11-19 18:41:56 +0800

    冒泡排序

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
 <?php
+/**
+ * https://mp.weixin.qq.com/s/vn3KiV-ez79FmbZ36SX9lg
+ * https://github.com/MisterBooo/LeetCodeAnimation
+ */
+//冒泡排序
+class sf{
+    var $arr = array();
+    function __construct(){
+        $this->arr =array(1,4,7,8,0,9,5);
+    }
+    //冒泡排序
+    public function BubbleSort (){
+        $len =$this->arr.length();
+        for($i=0;$i<$len;$i++ ){
+            for($j=0;$j<$len-1;$j++){
+                
+            }
+        }
+    }
+}
 
 die("ddddddd");
